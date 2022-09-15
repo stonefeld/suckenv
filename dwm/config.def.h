b7b3c61df10f57e536c49c740f488c45246643b2
stonefeld <theo.stanfield@gmail.com>
2022-09-15 15:45:07 -0300

    Added anysize patch to st

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -89,10 +89,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", dmenu_norm_bg, "-nf", dmenu_norm_fg, "-sb", dmenu_sel_bg, "-sf", dmenu_sel_fg, "-i", NULL };
-// static const char *termcmd[]  = { "st", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
